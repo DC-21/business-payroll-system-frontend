1dc7aa814eb66cdbd419e718d1381ed5ad0ddd93
DC-21 <cholahkuboko@gmail.com>
2023-08-11 14:31:45 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,9 @@
 4
 const Hero = () => {
   return (
-    <div className='w-full h-screen flex rounded bg-blue-200'>Hero</div>
+    <div className='w-full h-screen justify-center items-center flex rounded bg-blue-200'>
+      <p className="text-[30px] text-blue-950">DiCE LABS Human Resource Management Web App</p>
+    </div>
   )
 }
 
