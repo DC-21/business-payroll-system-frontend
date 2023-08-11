59db030d01abb03204dce53d7dfd9b3778db23b7
DC-21 <cholahkuboko@gmail.com>
2023-08-11 12:05:40 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
-
+4
 const Hero = () => {
   return (
-    <div className='w-full h-screen flex mt-2 rounded bg-green-300'>Hero</div>
+    <div className='w-full h-screen flex rounded mb-4'>Hero</div>
   )
 }
 
