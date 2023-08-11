50c15a9cc069b044174f99496df251b4cd07a5e5
DC-21 <cholahkuboko@gmail.com>
2023-08-11 11:18:48 +0200

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,37 +7,37 @@ const Sidebar = () => {
       <div className="justify-center items-center h-[576px] w-full py-6 px-2">
         <div className="justify-between rounded bg-blue-950 py-4 px-2 flex flex-col w-full h-full items-center">
           <li className="list-none flex flex-col gap-2">
-            <a className="cursor-pointer text-white flex w-full items-center gap-2 justify-start">
+            <a className="cursor-pointer rounded hover:bg-blue-300 hover:text-blue-950 text-white flex w-full items-center gap-2 justify-start">
               <ion-icon size="large" name="grid-outline"></ion-icon>
               <p>Dashboard</p>
             </a>
-            <a className="cursor-pointer text-white flex w-full items-center gap-2 justify-start">
+            <a className="cursor-pointer rounded hover:bg-blue-300 hover:text-blue-950 text-white flex w-full items-center gap-2 justify-start">
               <ion-icon size="large" name="wallet-outline"></ion-icon>
               <p>Payroll</p>
             </a>
-            <a className="cursor-pointer text-white flex w-full items-center gap-2 justify-start">
+            <a className="cursor-pointer rounded hover:bg-blue-300 hover:text-blue-950 text-white flex w-full items-center gap-2 justify-start">
               <ion-icon size="large" name="person-add-outline"></ion-icon>
               <p>Recruit</p>
             </a>
-            <a className="cursor-pointer text-white flex w-full items-center gap-2 justify-start">
+            <a className="cursor-pointer rounded hover:bg-blue-300 hover:text-blue-950 text-white flex w-full items-center gap-2 justify-start">
               <ion-icon size="large" name="receipt-outline"></ion-icon>
               <p>Reports</p>
             </a>
-            <a className="cursor-pointer text-white flex w-full items-center gap-2 justify-start">
+            <a className="cursor-pointer rounded hover:bg-blue-300 hover:text-blue-950 text-white flex w-full items-center gap-2 justify-start">
               <ion-icon size="large" name="card-outline"></ion-icon>
               <p>Billing</p>
             </a>
-            <a className="cursor-pointer text-white flex w-full items-center gap-2 justify-start">
+            <a className="cursor-pointer rounded hover:bg-blue-300 hover:text-blue-950 text-white flex w-full items-center gap-2 justify-start">
               <ion-icon size="large" name="calendar-outline"></ion-icon>
               <p>Calendar</p>
             </a>
-            <a className="cursor-pointer text-white flex w-full items-center gap-2 justify-start">
+            <a className="cursor-pointer rounded hover:bg-blue-300 hover:text-blue-950 text-white flex w-full items-center gap-2 justify-start">
               <ion-icon size="large" name="list-circle-outline"></ion-icon>
               <p>Departments</p>
             </a>
           </li>
           <li className="list-none">
-            <a className="cursor-pointer text-white flex w-full items-center gap-2 justify-start mr-9">
+            <a className="cursor-pointer rounded hover:bg-blue-300 hover:text-blue-950 text-white flex w-full items-center gap-2 justify-start mr-9">
               <ion-icon size="large" name="settings-outline"></ion-icon>
               <p>Settings</p>
             </a>
