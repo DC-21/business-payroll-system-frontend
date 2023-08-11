9f5c02e8154cd1653a4a67b93dd1730d9533fdf1
DC-21 <cholahkuboko@gmail.com>
2023-08-11 11:59:04 +0200

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 
 const Navbar = () => {
   return (
-    <div className='w-full flex top-0 py-7 bg-slate-500'>
-      <div className="flex w-full px-4">
+    <div className='w-full flex top-0 py-2'>
+      <div className="flex w-full rounded py-7 px-4 bg-slate-500">
         <a>
         <ion-icon size="large" name="person-circle-outline"></ion-icon>
         </a>
