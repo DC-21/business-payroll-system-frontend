d500d2e672127cad8fedb492804a9fdab93966bd
DC-21 <cholahkuboko@gmail.com>
2023-08-11 14:49:45 +0200

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
-import logo from '../images/logo.png'
+import logo from "../images/logo.png";
 
 const Sidebar = () => {
   return (
     <div className="h-screen w-[170px] bg-white items-center flex px-2 py-2 justify-between flex-col">
       <div className="justify-center rounded bg-blue-950 w-full flex">
-        <img className='h-[95px] w-[100px]' src={logo} alt="logo"/>
+        <img className="h-[95px] w-[100px]" src={logo} alt="logo" />
       </div>
       <div className="justify-center items-center h-[576px] w-full py-2">
         <div className="justify-between rounded bg-blue-950 py-4 flex flex-col w-full h-full items-center">
@@ -37,11 +37,15 @@ const Sidebar = () => {
               <ion-icon size="large" name="list-circle-outline"></ion-icon>
               <p>Departments</p>
             </a>
-          </li>
-          <li className="list-none">
-            <a className="cursor-pointer rounded hover:bg-blue-300 hover:text-blue-950 text-white flex w-full items-center gap-2 justify-start mr-9">
+            <a className="cursor-pointer rounded hover:bg-blue-300 hover:text-blue-950 text-white flex w-full items-center gap-2 justify-start">
               <ion-icon size="large" name="settings-outline"></ion-icon>
               <p>Settings</p>
+            </a>
+          </li>
+          <li className="list-none">
+          <a className="cursor-pointer rounded hover:bg-blue-300 hover:text-blue-950 text-white flex w-full items-center gap-2 justify-start mr-9">
+              <ion-icon size="large" name="log-out-outline"></ion-icon>
+              <p>Logout</p>
             </a>
           </li>
         </div>
