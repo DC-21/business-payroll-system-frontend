e0c35f634205edc9f88fb98b81dece348438d9df
DC-21 <cholahkuboko@gmail.com>
2023-08-12 17:06:14 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,12 @@ import Home from "./pages/Home";
 import Payroll from "./components/Payroll";
 import Billing from "./components/Billing";
 import Calendar from "./components/Calendar";
+import AllDepartments from "./Tables/AllDepartments";
+import Management from "./Tables/Management";
+import Security from "./Tables/Security";
+import Cleaners from './Tables/Cleaners';
+import IT from './Tables/IT';
+import Finance from './Tables/Finance';
 
 const App = () => {
   return (
@@ -25,6 +31,13 @@ const App = () => {
             <Route path="/calendar" element={<Calendar/>}/>
             <Route path="/departments" element={<Department />} />
             <Route path="/settings" element={<Settings/>}/>
+
+            <Route path="/all-departments" element={<AllDepartments/>}/>
+            <Route path="/management-department" element={<Management/>}/>
+            <Route path="/it-department" element={<IT/>}/>
+            <Route path="/finance-department" element={<Finance/>}/>
+            <Route path="/security-department" element={<Security/>}/>
+            <Route path="/cleaners-department" element={<Cleaners/>}/>
           </Routes>
         </div>
       </div>
--- src/Tables/Security.jsx
+++ src/Tables/Security.jsx
@@ -1,47 +1,52 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Security = () => {
-    const[security,setSecurity]=useState({});
+  const [security, setSecurity] = useState([]);
 
-    useEffect(()=>{
-        axios.get("http://localhost:3000/security").then((response)=>{
-            const data = response.data.security;
-            setSecurity(data);
-            console.log("fetched securities:",data);
-        }).catch((error) => {
-            console.log(error);
-          });
-    },[]);
-    
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/security")
+      .then((response) => {
+        const data = response.data.security;
+        setSecurity(Object.values(data)); // Convert object values to array
+        console.log("fetched securities:", data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="flex w-full justify-center items-center">
-        <table>
-            <thead>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Nrc</th>
-                <th>Salary</th>
-                <th>Account Number</th>
-            </thead>
-            <tbody>
-                {security.map((security)=>(
-                    <tr key={security.id}>
-                        <td>{security.name}</td>
-                        <td>{security.position}</td>
-                        <td>{security.email}</td>
-                        <td>{security.phone}</td>
-                        <td>{security.nrc}</td>
-                        <td>{security.salary}</td>
-                        <td>{security.account_no}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+      <table>
+        <thead>
+          <tr>
+            <th className="border border-blue-900 bg-white px-4 py-2">Name</th>
+            <th className="border border-blue-900 bg-white px-4 py-2">Position</th>
+            <th className="border border-blue-900 bg-white px-4 py-2">Email</th>
+            <th className="border border-blue-900 bg-white px-4 py-2">Phone</th>
+            <th className="border border-blue-900 bg-white px-4 py-2">Nrc</th>
+            <th className="border border-blue-900 bg-white px-4 py-2">Salary</th>
+            <th className="border border-blue-900 bg-white px-4 py-2">Account Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {security.map((security) => (
+            <tr key={security.id}>
+              <td className="border border-blue-900 bg-white px-4 py-2">{security.name}</td>
+              <td className="border border-blue-900 bg-white px-4 py-2">{security.position}</td>
+              <td className="border border-blue-900 bg-white px-4 py-2">{security.email}</td>
+              <td className="border border-blue-900 bg-white px-4 py-2">{security.phone}</td>
+              <td className="border border-blue-900 bg-white px-4 py-2">{security.nrc}</td>
+              <td className="border border-blue-900 bg-white px-4 py-2">{security.salary}</td>
+              <td className="border border-blue-900 bg-white px-4 py-2">{security.account_no}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Security
+export default Security;
