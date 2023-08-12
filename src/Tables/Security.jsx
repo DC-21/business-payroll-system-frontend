0485cd94dc23044334ea6db33b429747d82f176a
DC-21 <cholahkuboko@gmail.com>
2023-08-12 16:48:39 +0200

    first commit

--- src/Tables/Security.jsx
+++ src/Tables/Security.jsx
@@ -14,7 +14,32 @@ const Security = () => {
           });
     },[]);
   return (
-    <div></div>
+    <div className="flex w-full justify-center items-center">
+        <table>
+            <thead>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Nrc</th>
+                <th>Salary</th>
+                <th>Account Number</th>
+            </thead>
+            <tbody>
+                {security.map((security)=>(
+                    <tr key={security.id}>
+                        <td>{security.name}</td>
+                        <td>{security.position}</td>
+                        <td>{security.email}</td>
+                        <td>{security.phone}</td>
+                        <td>{security.nrc}</td>
+                        <td>{security.salary}</td>
+                        <td>{security.account_no}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
   )
 }
 
