db4b42ab2013b5faf6037b0dcc71961729c7d998
DC-21 <cholahkuboko@gmail.com>
2023-08-12 11:40:52 +0200

    first commit

--- src/components/Departments.jsx
+++ src/components/Departments.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom"
+import Department from '../pages/Department';
 
 const Departments = () => {
   return (
-    <div className="w-full flex bg-blue-200">hello</div>
+    <div className="w-full flex bg-blue-200 justify-center items-center">
+      <div className="flex flex-col w-full justify-center items-center">
+        <p className="text-blue-950 text-2xl font-semibold">Departments</p>
+        <p className="text-blue-900 text-xl mt-4">View all Departments Records</p>
+      </div>
+    </div>
   )
 }
 
