b5eb16b45a95899080fa9ed0812d49cb68eadeff
DC-21 <cholahkuboko@gmail.com>
2023-08-12 12:18:56 +0200

    first commit

--- src/components/Departments.jsx
+++ src/components/Departments.jsx
@@ -1,15 +1,22 @@
-import { Link } from "react-router-dom"
-import Department from '../pages/Department';
-
 const Departments = () => {
   return (
-    <div className="w-full flex bg-blue-200 justify-center items-center">
+    <div className="w-full flex flex-col bg-blue-200 justify-center items-center">
       <div className="flex flex-col w-full justify-center items-center">
         <p className="text-blue-950 text-2xl font-semibold">Departments</p>
-        <p className="text-blue-900 text-xl mt-4">View all Departments Records</p>
+        <p className="text-blue-900 text-xl mt-4">
+          View all Departments Records.
+        </p>
       </div>
+      <li className="w-full flex gap-4 justify-center items-center list-none text-blue-900">
+        <a>All Departments</a>
+        <a>Management</a>
+        <a>IT Departments</a>
+        <a>Finance Departments</a>
+        <a>Security Departments</a>
+        <a>Cleaners Departments</a>
+      </li>
     </div>
-  )
-}
+  );
+};
 
-export default Departments
+export default Departments;
