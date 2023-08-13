bae6ff5012aa890d94de2e980db4aab6f4eae41f
DC-21 <cholahkuboko@gmail.com>
2023-08-13 17:22:05 +0200

    first commit

--- src/components/Departments.jsx
+++ src/components/Departments.jsx
@@ -1,6 +1,11 @@
-import Security from "../Tables/Security";
+import { useState } from "react";
+import AllDepartments from "../Tables/AllDepartments";
+// import Security from "../Tables/Security";
+import { Link } from 'react-router-dom';
+import Management from '../Tables/Management';
 
 const Departments = () => {
+  const[department,setDepartment]=useState('')
   return (
     <div className="w-full flex flex-col bg-blue-200">
       <div className="flex flex-col items-center py-8">
@@ -10,15 +15,16 @@ const Departments = () => {
         </p>
       </div>
       <ul className="w-full flex gap-4 justify-center items-center list-none text-white">
-        <li className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">All Departments</li>
-        <li className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Management</li>
-        <li className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">IT Departments</li>
-        <li className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Finance Departments</li>
-        <li className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Security Departments</li>
-        <li className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Cleaners Departments</li>
+        <button onClick={()=>setDepartment("All")} className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">All Departments</button>
+        <button onClick={()=>setDepartment("Management")} className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Management</button>
+        <Link to="/it-department" className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">IT Departments</Link>
+        <Link to="/finance-department" className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Finance Departments</Link>
+        <Link to="/security-department" className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Security Departments</Link>
+        <Link to="/cleaners-department" className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Cleaners Departments</Link>
       </ul>
       <div className="mt-4">
-      <Security/>
+        {department==="All"?(<AllDepartments/>):null}
+        {department==="Management"?(<Management/>):null}
       </div>
     </div>
   );
