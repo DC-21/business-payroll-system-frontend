129435246db2fc21531fe07d9b184435653c0da5
DC-21 <cholahkuboko@gmail.com>
2023-08-14 11:17:58 +0200

    first commit

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,4 +1,3 @@
-
 const Calendar = () => {
   const currentDate = new Date();
   const currentDay = currentDate.getDate();
@@ -18,14 +17,14 @@ const Calendar = () => {
   const renderCalendar = () => {
     const calendar = [];
     for (let i = 0; i < firstDayIndex; i++) {
-      calendar.push(<div key={i} className="calendar-day"></div>);
+      calendar.push(<div key={i} className="calendar-day empty-day"></div>);
     }
 
     for (let i = 1; i <= daysInMonth; i++) {
       const isCurrentDay = i === currentDay;
       const dayClassName = isCurrentDay
-        ? 'calendar-day current-day'
-        : 'calendar-day';
+        ? "calendar-day current-day bg-red-500"
+        : "calendar-day";
 
       calendar.push(
         <div key={i} className={dayClassName}>
@@ -38,16 +37,25 @@ const Calendar = () => {
   };
 
   return (
-    <div className="w-full flex bg-blue-200">
-      <div className="calendar-header">
-        <h2 className="calendar-title">
-          {currentDate.toLocaleDateString('en-US', {
-            year: 'numeric',
-            month: 'long',
-          })}
-        </h2>
+    <div className="w-full bg-blue-200 p-4 rounded">
+      <div className="w-full grid grid-cols-7 gap-1">
+        <div className="w-full flex flex-col">
+            <h2 className="text-center text-lg mb-4">
+              {currentDate.toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+              })}
+            </h2>
+            <div className="w-full grid grid-cols-7 gap-1">
+              {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((day, index) => (
+                <div key={index} className="calendar-day header-day px-4">
+                  {day}
+                </div>
+              ))}
+              {renderCalendar()}
+            </div>
+        </div>
       </div>
-      <div className="grid grid-cols-7 gap-1">{renderCalendar()}</div>
     </div>
   );
 };
