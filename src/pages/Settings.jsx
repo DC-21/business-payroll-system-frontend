10548e2d1a72628d90c47eec3d5512c96e13711b
DC-21 <cholahkuboko@gmail.com>
2023-08-14 09:58:54 +0200

    first commit

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -7,28 +7,28 @@ import Security from '../Tables/Security';
 import Cleaners from '../Tables/Cleaners';
 
 const Departments = () => {
-  const[department,setDepartment]=useState('')
+  const[settings,setSettings]=useState('')
   return (
     <div className="w-full h-screen flex flex-col bg-blue-200">
       <div className="flex flex-col items-center py-8">
         <p className="text-blue-950 text-2xl font-semibold">User Settings</p>
       </div>
-      <ul className="w-full flex gap-4 justify-center items-center list-none text-white">
-        <button onClick={()=>setDepartment("All")} className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Profile</button>
-        <button onClick={()=>setDepartment("Management")} className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Password</button>
-        <button onClick={()=>setDepartment("IT")} className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Team</button>
-        <button onClick={()=>setDepartment("Finance")} className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Permissions</button>
-        <button onClick={()=>setDepartment("Security")} className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Security</button>
-        <button onClick={()=>setDepartment("Security")} className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Privacy</button>
-        <button onClick={()=>setDepartment("Cleaners")} className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Activity Log</button>
+      <ul className="w-full flex gap-4 justify-start px-2 items-center list-none text-white">
+        <button onClick={()=>setSettings("All")} className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Profile</button>
+        <button onClick={()=>setSettings("Management")} className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Password</button>
+        <button onClick={()=>setSettings("IT")} className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Team</button>
+        <button onClick={()=>setSettings("Finance")} className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Permissions</button>
+        <button onClick={()=>setSettings("Security")} className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Security</button>
+        <button onClick={()=>setSettings("Security")} className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Privacy</button>
+        <button onClick={()=>setSettings("Cleaners")} className="py-3 px-2 rounded bg-blue-950 hover:bg-blue-900 cursor-pointer">Activity Log</button>
       </ul>
       <div className="mt-4">
-        {department==="All"?(<AllDepartments/>):null}
-        {department==="Management"?(<Management/>):null}
-        {department==="IT"?(<IT/>):null}
-        {department==="Finance"?(<Finance/>):null}
-        {department==="Security"?(<Security/>):null}
-        {department==="Cleaners"?(<Cleaners/>):null}
+        {settings==="All"?(<AllDepartments/>):null}
+        {settings==="Management"?(<Management/>):null}
+        {settings==="IT"?(<IT/>):null}
+        {settings==="Finance"?(<Finance/>):null}
+        {settings==="Security"?(<Security/>):null}
+        {settings==="Cleaners"?(<Cleaners/>):null}
       </div>
     </div>
   );
