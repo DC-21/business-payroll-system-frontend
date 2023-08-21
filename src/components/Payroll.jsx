72644e3316fce3ca37d4b951b6d27d09062cbdb8
DC-21 <cholahkuboko@gmail.com>
2023-08-21 19:00:22 +0200

    first commit

--- src/components/Payroll.jsx
+++ src/components/Payroll.jsx
@@ -1,13 +1,13 @@
 const Payroll = () => {
   return (
-    <div className="w-full bg-blue-200 flex">
-      <div className="w-full py-6 px-2 flex gap-4">
+    <div className="w-full flex">
+      <div className="w-full py-6 px-2 flex gap-4 bg-blue-200 rounded">
         <li className="gap-4 flex w-full">
-          <a className="py-4 px-4 bg-blue-900 rounded text-white items-center">Active</a>
-          <a className="py-4 px-4 bg-blue-900 rounded text-white">Pending</a>
-          <a className="py-4 px-4 bg-blue-900 rounded text-white">Monthly</a>
-          <a className="py-4 px-4 bg-blue-900 rounded text-white">Active</a>
-          <a className="py-4 px-4 bg-blue-900 rounded text-white">Active</a>
+          <a className="py-4 px-4 bg-blue-950 hover:bg-blue-900 rounded cursor-pointer text-white items-center">Active</a>
+          <a className="py-4 px-4 bg-blue-950 hover:bg-blue-900 rounded cursor-pointer text-white">Pending</a>
+          <a className="py-4 px-4 bg-blue-950 hover:bg-blue-900 rounded cursor-pointer text-white">Monthly</a>
+          <a className="py-4 px-4 bg-blue-950 hover:bg-blue-900 rounded cursor-pointer text-white">Active</a>
+          <a className="py-4 px-4 bg-blue-950 hover:bg-blue-900 rounded cursor-pointer text-white">Active</a>
         </li>
       </div>
     </div>
