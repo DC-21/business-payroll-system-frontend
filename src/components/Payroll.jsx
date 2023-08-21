25f98c0016a221aef905a53999eb7aa355cdd489
DC-21 <cholahkuboko@gmail.com>
2023-08-21 18:45:36 +0200

    first commit

--- src/components/Payroll.jsx
+++ src/components/Payroll.jsx
@@ -1,9 +1,9 @@
 const Payroll = () => {
   return (
-    <div className="h-screen w-full bg-blue-200 flex">
+    <div className="w-full bg-blue-200 flex">
       <div className="w-full py-6 px-2 flex gap-4">
         <li className="gap-4 flex w-full">
-          <a className="py-4 px-4 bg-blue-900 text-white">Active</a>
+          <a className="py-4 px-4 bg-blue-900 text-white items-center">Active</a>
           <a className="py-4 px-4 bg-blue-900 text-white">Pending</a>
           <a className="py-4 px-4 bg-blue-900 text-white">Monthly</a>
           <a className="py-4 px-4 bg-blue-900 text-white">Active</a>
